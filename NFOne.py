35c0939be8c4916caf1996729b10a487782fc8cc
Edd Osorio <osorio.edd@gmail.com>
2015-06-26 13:43:48 -0500

    Update all experiments

--- NFOne.py
+++ NFOne.py
@@ -23,8 +23,9 @@
 #  
 import time
 
+
 def verificar_primo(numero):
-	rango = range(2,numero)
+	rango = range(2, numero)
 	if numero == 1:
 		return False
 	elif numero == 2:
@@ -38,33 +39,34 @@ def verificar_primo(numero):
 				if elementos == numero - 1:
 					return numero
 
+
 def main():
 	t0 = time.time()
-	archivo = open("NFOne.txt","w+")
-	i=100
-	
+	archivo = open("NFOne.txt", "w+")
+	i = 100
+
 	while i < 9999:
-		c=c1=0
+		c = c1 = 0
 		if i < 1000:
 			s = "0" + str(i)
 		else:
 			s = str(i)
-		
+
 		for e in s:
 			c += s.count(e)
 			c -= 1
-			c1 += s.count(str(int(e)+1))
-			
-		if c == 0 and c1==0:
-			#archivo.write("%s\t%i\n" % (s,c1))
+			c1 += s.count(str(int(e) + 1))
+
+		if c == 0 and c1 == 0:
+			# archivo.write("%s\t%i\n" % (s,c1))
 			if verificar_primo(int(s)):
 				archivo.write("%s\n" % s)
-		i+=1
-	
+		i += 1
+
 	archivo.write("%s" % (time.time() - t0))
-	
 	archivo.close()
 	return 0
+
 
 if __name__ == '__main__':
 	main()
--- armstrong.py
+++ armstrong.py
@@ -10,11 +10,11 @@ test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
 	x = len(test) - 1
 	a = map(int,test[:x])
-	
+
 	b = 0
 	for i in a:
 		b += i ** x
-	
+
 	if b == int(test[:x]):
 		print True
 	else:	
